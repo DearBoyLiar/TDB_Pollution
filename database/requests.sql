7085f2498ccefe986987b22b52b281c5eeef6cad
DearBoyLiar <azzouza.william@gmail.com>
2019-03-22 09:49:28 +0100

    FINAL PUSH

--- database/requests.sql
+++ database/requests.sql
@@ -1,4 +1,4 @@
---Récupération du tf cumulé de chaque mot
+--Q1
 Select TOP 30 label_word, sum(tf)
 from belong b, word w 
 where w.id_word = b.id_word
@@ -7,7 +7,7 @@ and label_word not in ('000')
 group by label_word
 order by 2 desc
 
---Avec une liste de pays
+--Q2
 select TOP 30 sum(tf), label_word, id_article
 from belong b, word w
 where b.id_word = w.id_word 
@@ -16,7 +16,7 @@ and b.is_country = 1
 group by label_word, id_article
 order by 1 desc
 
--- par journaux
+-- Q3
 select TOP 30 sum(tf), label_word, name_newspaper
 from belong b, word w, article a, newspaper n 
 where b.id_word = w.id_word 
@@ -27,7 +27,7 @@ and label_word not in ('000')
 group by label_word, name_newspaper
 order by 1 desc
 
---par date / pays
+--Q4
 select TOP 30 sum(tf), label_word, left(publication_date,4) 
 from belong b, word w, article a 
 where b.id_word = w.id_word 
@@ -38,7 +38,7 @@ and left(publication_date,4)  between '1950' and '2019'
 group by label_word, left(publication_date,4) 
 order by 1 desc
 
---Nombre de mots par date
+--Q5
 select count(label_word), left(publication_date, 4)
 from belong b, word w, article a
 where b.id_word = w.id_word 
@@ -47,7 +47,7 @@ and a.id_article <> 542
 group by left(publication_date, 4)
 order by 2 desc
 
---mot pertinent
+--Q6
 select label_word, a.id_article, tf, left(publication_date, 4)
 from belong b, word w, article a
 where b.id_word = w.id_word 
@@ -57,7 +57,7 @@ and label_word in ('ocean', 'plastiques', 'plastique', 'mer', 'dechets', 'dechet
 'microplastiques', 'microplastiques')
 order by 4 desc
 
--- pays par mois avec label associé à chaque tranche de date
+-- Q7
 select distinct label_word, left(publication_date, 7),
 case when  left(publication_date, 7) between '2010-01' and '2019-03' THEN 1
 when  left(publication_date, 7) between '2000-01' and '2010-01' THEN 2
@@ -75,7 +75,7 @@ and is_country = 1
 order by 2 desc
 
 
--- pays par année avec label associé à chaque tranche de date
+-- Q8
 select distinct label_word, left(publication_date, 4),
 case when  left(publication_date, 4) between '2010' and '2019' THEN 1
 when  left(publication_date, 4) between '2000' and '2010' THEN 2
@@ -92,7 +92,7 @@ and b.id_article <> 542
 and is_country = 1
 order by 2 desc
 
---nb de pays par mois
+--Q9
 select a.word, a.date_pub,
 case when  left(a.date_pub, 7) between '2010-01' and '2019-01' THEN 1
 when  left(a.date_pub, 7) between '2000-01' and '2010-01' THEN 2
@@ -114,7 +114,7 @@ group by left(publication_date, 7)
 order by 2 desc
 ) a
 
--- nb de pays par année
+-- Q10
 select a.word, a.date_pub,
 case when  left(a.date_pub, 4) between '2010' and '2019' THEN 1
 when  left(a.date_pub, 4) between '2000' and '2010' THEN 2
@@ -136,7 +136,7 @@ group by left(publication_date, 4)
 ) a
 order by 2 desc
 
---Mots qui apparaissent le plus dans les descriptions d'article
+--Q11
 Select TOP 8 count(b.id_word), label_word
 from belong b, word w
 where w.id_word = b.id_word
@@ -145,7 +145,7 @@ and b.is_description = 1
 group by label_word
 order by 1 desc
 
---Mots qui apparaissent le plus dans les titres d'articles
+--Q12
 Select TOP 8 count(b.id_word), label_word
 from belong b, word w
 where w.id_word = b.id_word
@@ -154,7 +154,7 @@ and b.is_title = 1
 group by label_word
 order by 1 desc
 
---Mots qui apparaissent le plus dans les titres et description d'articles
+--Q13
 Select TOP 8 count(b.id_word), label_word
 from belong b, word w
 where w.id_word = b.id_word
